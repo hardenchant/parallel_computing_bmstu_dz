8f1f038de944b994451e079ed7f6f5146bd6fd94
g.lysov <17965301+hardenchant@users.noreply.github.com>
2020-12-21 14:26:56 +0300

    rm garbage

--- generate.py
+++ generate.py
@@ -1,41 +1,11 @@
 from random import randrange, sample
 
-_ = {
-    "automates": [
-        {
-            "name": "auto number one",
-            "graph": {
-                "1": [["2", "3"], "lambda x, y, prev: x + y + prev + 1"],
-                "2": [["1"], "lambda x, prev: x + prev + 2"],
-                "3": [["1"], "lambda x, prev: x + prev + 3"],
-            },
-            "result": {"vertices": ["3"], "function": "lambda v3: v3 + 21"},
-        }
-    ],
-    "start_states": [[0, 0, 0]],
-    "result": [24, 28],
-}
-
-_ = {
-    "automates": [
-        {
-            "name": "0",
-            "graph": {"0": [["0"], "sum"], "1": [[], "sum"]},
-            "result": {"vertices": [], "function": "sum"},
-        }
-    ],
-    "start_states": [[0, 0]],
-}
-
 
 def generate_graph(sz: int):
     graph = {}
     for i in range(sz):
-        # nodes = sample(range(sz), randrange(sz))
         nodes = sample(range(sz), 5)
-
         graph[str(i)] = [list(map(str, nodes)), "lambda *args: sum(args) % 100"]
-
     return graph
 
 
@@ -47,9 +17,6 @@ def generate_fsm(sz: int, graph_sz: int):
             "result": {
                 "vertices": list(
                     map(str, sample(range(graph_sz), randrange(graph_sz)))
-                    # map(str, sample(range(graph_sz), graph_sz/2))
-                    # map(str, range(graph_sz))
-
                 ),
                 "function": "lambda *args: sum(args) % 100",
             },
@@ -60,10 +27,10 @@ def generate_fsm(sz: int, graph_sz: int):
     return res
 
 
-def generate_data(aut_sz, graph_sz):
+def generate_data(aut_sz, graph_sz, max_start_value=2):
     return {
         "automates": generate_fsm(aut_sz, graph_sz),
         "start_states": [
-            [randrange(2) for _ in range(graph_sz)] for _ in range(aut_sz)
+            [randrange(max_start_value) for _ in range(graph_sz)] for _ in range(aut_sz)
         ],
     }
